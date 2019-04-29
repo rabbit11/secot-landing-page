855388c7351f8bd46789f4796c65518d293f056c
Pedro Coelho <pedro.coelho@dcomp.sor.ufscar.br>
2019-04-29 00:10:46 -0300

    mudanças finais no tamanho do texto

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ var rate = setInterval(function () {
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     // Display the result in the element with id="demo"
-//     count.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
+    //     count.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
 
     dias.innerHTML = days;
     horas.innerHTML = hours;
@@ -33,12 +33,50 @@ var rate = setInterval(function () {
 }, 10);
 
 $(function () {
-        $(document).scroll(function () {
-                var $nav = $(".navbar");
-                $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-                $nav.toggleClass('navbar-dark', $(this).scrollTop() > $nav.height());
-                // $nav.toggleClass('shadow-lg', $(this).scrollTop() > $nav.height());
-                // $nav.toggleClass('border-bottom border-danger', $(this).scrollTop() > $nav.height());
-        });
+    $(document).scroll(function () {
+        var $nav = $(".navbar");
+        $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+        $nav.toggleClass('navbar-dark', $(this).scrollTop() > $nav.height());
+        // $nav.toggleClass('shadow-lg', $(this).scrollTop() > $nav.height());
+        // $nav.toggleClass('border-bottom border-danger', $(this).scrollTop() > $nav.height());
+    });
 });
 
+$(document).ready(function () {
+    // Select all links with hashes
+    $('a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function (event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
+                &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function () {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        };
+                    });
+                }
+            }
+        });
+})
